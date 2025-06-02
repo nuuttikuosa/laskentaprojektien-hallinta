47ea43662aacbe8e4116967a67e5809113e565d6
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2025-06-03 02:23:35 +0300

    Generating tasks added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import redirect, render_template, request, session, abort
 import config
 import users
 import projects
+import constants
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -16,11 +17,11 @@ def index():
 @app.route("/project/<int:project_id>")
 def show_project(project_id):
     project = projects.get_project(project_id)
-    #tasks = projects.get_tasks(thread_id)
+    tasks = projects.get_tasks(project_id)
 
     project_parameters= projects.get_project_parameters(project_id)
 
-    return render_template("project.html", project=project, project_parameters = project_parameters)
+    return render_template("project.html", project=project, project_parameters = project_parameters, tasks=tasks)
 
 @app.route("/remove/<int:project_id>", methods=["GET", "POST"])
 def remove_project(project_id):
@@ -31,7 +32,7 @@ def remove_project(project_id):
 
     if request.method == "POST":
         if "continue" in request.form:
-            projects.update_project_status(project["id"], 4)
+            projects.update_project_status(project["id"], constants.PROJECT_STATUS_DELETED)
         return redirect("/project/" + str(project["id"]))
 
 @app.route("/hold/<int:project_id>", methods=["GET", "POST"])
@@ -43,7 +44,7 @@ def set_project_on_hold(project_id):
 
     if request.method == "POST":
         if "continue" in request.form:
-            projects.update_project_status(project["id"], 3)
+            projects.update_project_status(project["id"], constants.PROJECT_STATUS_ON_HOLD)
         return redirect("/project/" + str(project["id"]))
 
 @app.route("/reactivate/<int:project_id>", methods=["GET", "POST"])
@@ -55,7 +56,7 @@ def reactivate_project(project_id):
 
     if request.method == "POST":
         if "continue" in request.form:
-            projects.update_project_status(project["id"], 1)
+            projects.update_project_status(project["id"], constants.PROJECT_STATUS_ONGOING)
         return redirect("/project/" + str(project["id"]))
 
 @app.route("/projects/search", methods=['GET'])
@@ -85,6 +86,33 @@ def edit(project_id):
         description = request.form["description"]
 
         projects.update_project(name, range_min, range_max, description, project_id)
+
+        return redirect("/project/" + str(project_id))
+
+@app.route("/edit/<int:project_id>/generate_tasks", methods=["GET", "POST"])
+def generate_tasks(project_id):
+
+    project = projects.get_project(project_id)
+    if session["user_id"] != project["user_id"]:
+        abort(403)
+
+    if request.method == "GET":
+        return render_template("generate_tasks.html", project=project)
+
+    if request.method == "POST":
+        min = int(request.form["min"])
+        max = int(request.form["max"])
+        if min < project["range_min"] or max > project["range_max"]:
+            return render_template("generate_tasks.html", project=project, error="Task numbers must be within the project range.")
+        if min > max:
+            return render_template("generate_tasks.html", project=project, error="Smallest task number cannot be larger than largest task number.")
+        if project["status"] != "Not Started":
+            return render_template("generate_tasks.html", project=project, error="Project must not be started to generate tasks.")
+        if min == max:
+            return render_template("generate_tasks.html", project=project, error="Smallest task number cannot be equal to largest task number.")
+
+        projects.generate_tasks(min, max, project_id)
+        projects.update_project_status(project_id, constants.PROJECT_STATUS_ONGOING)
 
         return redirect("/project/" + str(project_id))
 
